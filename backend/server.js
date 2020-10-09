717690e00914b9c53e30a7955d73277a007cde02
Thomas McVay <thomasmcvay@Thomass-iMac.home>
2020-10-09 10:09:26 +0100

    add user routes, auth, jwt

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const productRoutes = require('./routes/productRoutes'); 
+const userRoutes = require('./routes/userRoutes'); 
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API running...')
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
